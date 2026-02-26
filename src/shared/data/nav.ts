4ed6187c4014acb88e3b9131a870062b3907da8c
deathcoding <ilgiz.ilishev@gmail.com>
2026-02-26 11:11:56 +0500

    remove achievment from navbar

--- src/shared/data/nav.ts
+++ src/shared/data/nav.ts
@@ -6,7 +6,6 @@ export const navItems = [
   { id: "faq", label: "Вопрос-ответ" },
   { id: "expertise", label: "Проведение экспертизы" },
   { id: "application", label: "Подача заявки" },
-  { id: "awards", label: "Награды и благодарности" },
   { id: "requisites", label: "Реквизиты" },
   { id: "contacts", label: "Контакты" },
 ] as const;
